0a6f954bd4d0e548da33ce1b3126b6d68ea77146
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-09-22 18:11:32 -0500

    adding the code for the project

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# README_GENERATOR
+
+    # Kushubh
+
+    ## Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contributors](#contributors)
+    * [Test](#test)
+    * [License](#license)
+    * [Questions](#questions)
+    
+
+
+    ## Description
+    we are in love
+
+    ## Installation 
+    undefined
+
+    ## Usage 
+    give love
+
+    ## Contributors
+    kush, shubhi
+
+    ## Test
+    give a kiss
+
+    ## License
+    MIT
+
+    ## Questions
+    Contact me:
+    Github:[shubhangim](https://github.com/DiLiubarets)
+    Email:[shubhimundhada@gmail.com](https://github.com/DiLiubarets)
--- index.js
+++ index.js
@@ -1,15 +1,125 @@
-// array of questions for user
-const questions = [
+// Getting the needed node functions 
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
 
-];
+const writeFile = util.promisify(fs.writeFile);
+// array of questions for user
+function promptUser() {
+    return inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Give some description of what your project is about",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "What steps are needed to install the project?",
+        name: "Install"
+    },
+    {
+        type: "input",
+        message: "Give instructions for usability.",
+        name: "Usage"
+    },
+    {
+        type: "input",
+        message: "Who were the contributors in the project?",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        message: "What is the best method to test your project?",
+        name: "Test"
+    },
+    {
+        type: "list",
+        message: "Select license suited to your project",
+        name: "License",
+        choices: [
+            "MIT",
+            "Mozilla",
+            "APACHE",
+            "GPL",
+            "ECLIPSE",
+            "BSD",
+            "PUBLIC DOMAIN",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your Github username",
+        name: "Username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "Email"
+    }
+    
+    ]);
+}
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function generateReadMe(data) {
+    return `
+    # ${data.Title}
+
+    ## Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contributors](#contributors)
+    * [Test](#test)
+    * [License](#license)
+    * [Questions](#questions)
+    
+
+
+    ## Description
+    ${data.Description}
+
+    ## Installation 
+    ${data.Installation}
+
+    ## Usage 
+    ${data.Usage}
+
+    ## Contributors
+    ${data.Contributors}
+
+    ## Test
+    ${data.Test}
+
+    ## License
+    ${data.License}
+
+    ## Questions
+    Contact me:
+    Github:[${data.Username}](https://github.com/DiLiubarets)
+    Email:[${data.Email}](https://github.com/DiLiubarets)`;
 }
 
 // function to initialize program
-function init() {
-
+async function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+      const readMe = generateReadMe(answers);
+  
+      await writeFile("README.md", readMe);
+  
+      console.log("Successfully wrote to README.md");
+    } catch(err) {
+      console.log(err);
+    }
 }
 
 // function call to initialize program
