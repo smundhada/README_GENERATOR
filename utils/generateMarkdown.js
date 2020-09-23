aeb1827097a014ca77ea05f1147a3d828be74991
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-09-22 23:14:39 -0500

    Updating generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.Title}
+---
 
-`;
+## Table of Contents
+---
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributors](#Contributors)
+* [Test](#Test)
+* [License](#License)
+* [Questions](#Questions)
+
+
+
+## Description
+---
+${data.Description}
+
+## Installation 
+---
+${data.Install}
+
+## Usage 
+---
+${data.Usage}
+
+## Contributors
+---
+${data.Contributors}
+
+## Test
+---
+${data.Test}
+
+## License
+---
+${data.License}
+
+## Questions
+---
+Github Link - https://github.com/${data.Username}
+Email - ${data.Email}`;
 }
 
-module.exports = generateMarkdown;
+module.exports.generateMarkdown = generateMarkdown;
